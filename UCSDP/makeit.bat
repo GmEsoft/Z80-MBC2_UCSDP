fe2c0b584f519f94506bef88a5d93f63ca00d352
michel.bernard <michel.bernard@advalvas.be>
2019-12-17 17:43:02 +0100

    - added missing utility datetime.c; - added comments and cleaned code.

--- UCSDP/makeit.bat
+++ UCSDP/makeit.bat
@@ -1,11 +1,6 @@
 @echo off
 set name=ucsdp
-echo $DATE	MACRO>datetime.asm
-echo 	DB	"%DATE%">>datetime.asm
-echo 	ENDM>>datetime.asm
-echo $TIME	MACRO>>datetime.asm
-echo 	DB	"%TIME%">>datetime.asm
-echo 	ENDM>>datetime.asm
+datetime>datetime.asm
 zmac\zmac %name%.asm --od %name% --oo cim,lst -c -s -g
 if errorlevel 1 pause && goto :eof
 copy %name%\%name%.cim autoboot.bin
