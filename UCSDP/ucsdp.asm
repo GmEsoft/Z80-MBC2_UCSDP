a95f6bd46d9361e65d48b0575f1749f1457cbc3d
michel.bernard <michel.bernard@advalvas.be>
2019-12-17 18:10:54 +0100

    UCSDP Loader 1.2 - Added disk images selection at boot time.

--- UCSDP/ucsdp.asm
+++ UCSDP/ucsdp.asm
@@ -1,5 +1,5 @@
 ;=============================================================================
-;	UCSD p-System Bootstrap Loader and SBIOS V1.1 for Z80-MBC2
+;	UCSD p-System Bootstrap Loader and SBIOS V1.2 for Z80-MBC2
 ;=============================================================================
 ;
 ;	Build:
@@ -11,8 +11,8 @@
 
 ;-----------------------------------------------------------------------------
 ; 	Settings
-DISK0	EQU	20			; First disk number in set
-DEBUG	EQU	0			; Debug mode
+DISK0	EQU	20		; First disk number in set
+DEBUG	EQU	0		; Debug mode
 
 ;-----------------------------------------------------------------------------
 ;	This auto-generated file defines $DATE and $TIME macros
@@ -20,21 +20,22 @@ DEBUG	EQU	0			; Debug mode
 
 CR	EQU	0DH
 LF	EQU	0AH
+ESC	EQU	1BH
 
 ;-----------------------------------------------------------------------------
 ;	Macro to break in Keil-like emulator
 $BREAK	MACRO
 	IF	DEBUG
-	DB	0EDH,0F5H		; Keil's emulator $BREAK opcode
+	DB	0EDH,0F5H	; Keil's emulator $BREAK opcode
 	ENDIF
 	ENDM
-					;
+				;
 
 ;-----------------------------------------------------------------------------
 ;	IPL entry
 	ORG	0
-	JP	BOOTZ80			; bootstrap routine
-	DC	3DH,0			; fill RST vector area with NULL
+	JP	BOOTZ80		; bootstrap routine
+	DC	3DH,0		; fill RST vector area with NULL
 
 ;-----------------------------------------------------------------------------
 ;	Bootstrap routine, executed at first IPL
@@ -42,15 +43,16 @@ $BREAK	MACRO
 BOOTZ80:
 	DI
 
-	LD	HL,$BIOS_BEGIN		; Install CBIOS in high memory (at first IPL)
-	LD	DE,BIOS			;
-	LD	BC,$BIOS_END-$BIOS_BEGIN;
-	LDIR				;
-
+	LD	HL,$BIOS_BEGIN	; Install CBIOS in high memory (at first IPL)
+	LD	DE,BIOS		;
+	LD	BC,$BIOS_END-$BIOS_BEGIN
+	LDIR			;
+	LD	SP,BIOS		; CBIOS GOES HERE, RESET THE STACK
+	CALL	SETUP		;
 
 ;-----------------------------------------------------------------------------
 ;	Second IPL entry point
-MAIN:					;LET'S BOOT UCSD PASCAL
+MAIN:				;LET'S BOOT UCSD PASCAL
 ;
 ;	  This program is a skeletal outline for a 128-byte primary
 ;	bootstrap for automatically booting to UCSD Pascal (tm).
@@ -68,17 +70,10 @@ MAIN:					;LET'S BOOT UCSD PASCAL
 ;	Adapted for Z80-MBC2, August 2019, Michel B.
 ;
 BOOT	EQU	8200H		; SECONDARY BOOTSTRAP LOADED HERE
-;MSIZE	EQU	64		; MEMORY SIZE FOR ASSEMBLY
-;BIAS	EQU	(MSIZE*1024)-01A00H
-;CBIOS	EQU	1500H+BIAS	; ORIGIN POINT
 SECNUM	EQU	16		; SECONDARY BOOTSTRAP IS 16 SECTORS LONG
 SECSEC	EQU	3		; SECONDARY BOOTSTRAP ON THIS SECTOR
 ;
-PBOOT:	LD	HL,BIOS		; CBIOS GOES HERE
-	LD	SP,HL		; RESET THE STACK
-
-	LD	HL,HELLO$	; Copyright Text
-	CALL	PUTS		; Display message (at first IPL only)
+PBOOT:	LD	SP,BIOS		; CBIOS GOES HERE, RESET THE STACK
 	LD	HL,READSEC$	; Reading sec bootstrap
 	CALL	PUTS		; Display message (at first IPL only)
 	LD	HL,BOOT		; LOAD BOOT BASE ADDRESS
@@ -186,8 +181,8 @@ READSEC$:
 ;	Copyright (C) 2019 by GmEsoft
 ;
 ;
-HELLO$:
-	DB	'64K UCSD p-System IV.0 SBIOS V1.1 for Z80-MBC2, '
+HELLO$:	DB	ESC,'H',ESC,'J'
+	DB	'64K UCSD p-System IV.0 SBIOS V1.2 for Z80-MBC2, '
 	DB	'Copyright (C) 2019 by GmEsoft'
 	DB	CR,LF
 	DB	'Build: '
@@ -195,6 +190,320 @@ HELLO$:
 	DB	' - '
 	$TIME			; Macro defining build time
 	DB	CR,LF,0
+
+
+;-----------------------------------------------------------------------------
+;	SETUP DISKS
+SETUP:	LD	HL,HELLO$	; Display welcome message
+	CALL	PUTS		;
+	LD	C,0		; Select DISK0
+	CALL	SELDSK		;
+	LD	C,0		; Select track 0
+	CALL	SETTRK		;
+	LD	C,128		; Select last sector of track 0
+	CALL	SETSEC		;
+	LD	BC,CFGBUF	; Use Config buffer
+	PUSH	BC		; Save buffer address
+	CALL	SETDMA		;
+	CALL	READ		; Read sector into config buffer
+	POP	HL		; Restore buffer address
+	PUSH	HL		; Copy to IX
+	POP	IX		;
+	LD	A,(IX+127)	; Check signature byte
+	CP	0A5H		;   Last sector's byte == 0A5h
+	JR	NZ,DOCFG	; If yes skip applying config
+	LD	DE,DSKTBL	; Read disk map table
+	LD	BC,4		; 4 bytes
+	LDIR			;
+	LD	A,(HL)		; Read keymap code
+	LD	(KEYMAP),A	;
+	LD	A,80H		; IOS:USER_KEY opcode
+	OUT	(1),A		; Send opcode
+	IN	A,(0)		; Read user key status
+	AND	1		;
+	JR	NZ,DOCFG	; Go to config if key pressed
+
+	LD	HL,DOCFG$	; Display 'Press USER key ...'
+	JP	PUTS		; And return
+
+;	Do configuration interactively with user
+DOCFG:
+	LD	HL,SCNDSK$	; Display 'Scanning disk: ['
+	CALL	PUTS		;
+	LD	B,50		; Prepare progress bar
+	LD	C,'-'		;
+SCAN1:	CALL	CONOUT		; Display 50 dashes
+	DJNZ	SCAN1		;
+	LD	C,']'		; and closing bracket
+	CALL	CONOUT		;
+	LD	HL,SCNDSK$+1	; Re-display 'Scanning disk: ['
+	CALL	PUTS		;
+
+	LD	A,0		; Init A = Disk image counter
+	LD	IY,DSKNAMES	; Pointer to disk names table
+
+LOOPDSK	PUSH	AF		; Save counter
+	LD	(IY),0		; Blank disk name
+	LD	HL,DSKTMP	; Use temporary slot for disk access
+	LD	(HL),A		; Map scanned image to slot 4
+	LD	C,4		; Select disk slot 4
+	CALL	SELDSK		;
+	LD	C,1		; Select track 1 containing directory
+	CALL	SETTRK		;
+	LD	C,9		; Select sector 9 containing vol name
+	CALL	SETSEC		;
+	LD	BC,DIRBUF	; Use directory sector buffer
+	PUSH	BC		; Save pointer
+	CALL	SETDMA		; Set buffer address
+	CALL	READ		; Read 1st directory sector
+	POP	HL		; Restore directory buffer address
+	OR	A		; Check for error
+	JR	NZ,DSKNOK	; Go and Skip image in case of error
+
+	LD	A,(HL)		; Check 1st 6 bytes of sector
+	INC	HL		; Bytes 0,1,3,4,5 must equal 0
+	OR	(HL)		;
+	INC	HL		;
+	LD	C,(HL)		; Get byte 2 in C
+	INC	HL		;
+	OR	(HL)		;
+	INC	HL		;
+	OR	(HL)		;
+	INC	HL		;
+	OR	(HL)		;
+	INC	HL		;
+	JR	NZ,DSKNOK	; Go and skip image if the 5 bytes are not all 0
+
+	LD	A,C		; Check byte 2
+	CP	6		; Must be equal to 6
+	JR	Z,DSKOK		; Get vol name if yes
+
+	CP	10		; ... or 10
+	JR	NZ,DSKNOK	; Skip if not
+
+;	Disk image OK => get vol name
+DSKOK:	LD	C,(HL)		; Get name length in C
+	INC	HL		; Bump ptr
+	PUSH	IY		; Names table entry to DE
+	POP	DE		;
+	LD	B,0		; BC = name length
+	LDIR			; Copy name
+	EX	DE,HL		; End name address to HL
+	LD	(HL),0		; Terminate name with 0
+
+;	Point to next disk
+DSKNOK:	LD	BC,16		; Bump names table ptr
+	ADD	IY,BC		;
+	POP	AF		; Restore disk images counter
+	INC	A		; Next disk image
+	BIT	0,A		; Is Counter even ?
+	JR	NZ,DSKNO1	; Skip if not
+	PUSH	AF		; Save counter
+	LD	C,'+'		; Display a '+' in the progress bar
+	CALL	CONOUT		;
+	POP	AF		; Restore counter
+DSKNO1:	CP	100		; Last disk image reached ?
+	JR	C,LOOPDSK	; Loop again if not
+
+;	Display disk names with numbers
+	LD	HL,AVAIL$	; Display 'Available volumes:'
+	CALL	PUTS		;
+	LD	B,0		; init B = Image counter
+	LD	C,B		; init C = columns counter
+	LD	HL,DSKNAMES	; init HL = vol names table pointer
+LISTDSK	LD	A,(HL)		; Is there a name ?
+	OR	A		;
+	JR	Z,NODSKNM	; Skip displaying vol name if not
+	PUSH	BC		; Save BC
+	LD	A,B		; Display image number
+	CALL	OUTDECA2	;
+	LD	C,':'		; Display ':'
+	CALL	CONOUT		;
+	CALL	PUTS		; Display name pointed by HL
+	LD	C,' '		;
+	LD	A,10		; Tabulate
+	SUB	L		;
+	AND	0FH		;
+	LD	B,A		; by displaying ' 's
+LISTTAB	CALL	CONOUT		;
+	DJNZ	LISTTAB		;
+	POP	BC		; Restore BC
+	INC	C		; Next column
+	LD	A,C		; Column counter to A
+	CP	6		; 6 columns shown
+	JR	C,NODSKNM	; go if not
+	LD	C,CR		; Move to next screen line
+	CALL	CONOUT		;
+	LD	C,LF		;
+	CALL	CONOUT		;
+	LD	C,0		; Reset column counter
+NODSKNM	LD	A,L		; Move vol names table ptr
+	OR	0FH		;   to next entry
+	LD	L,A		;
+	INC	HL		;
+	INC	B		; Increment vol number
+	LD	A,B		;
+	CP	100		; Last vol reached
+	JR	C,LISTDSK	; Loop if not
+	LD	HL,SELDSK$	; Display 'Select volume numbers ...'
+	CALL	PUTS		;
+	LD	HL,DSKTBL	; Init disk mapping table ptr
+	LD	B,4		; Number of entries = 4
+	LD	C,4		; first p-System Logical unit number
+
+LOOPSEL	PUSH	BC		; Save counters
+	LD	C,CR		; Cursor to beginning of line
+	CALL	CONOUT		;
+	LD	C,'#'		; Display '#'
+	CALL	CONOUT		;
+	POP	BC		; Restore counters
+	PUSH	BC		; And save them again
+	LD	A,C		; Display logical unit number
+	CALL	OUTDECA2	;
+	LD	C,' '		; Display ' '
+	CALL	CONOUT		;
+	LD	A,(HL)		; Display image number
+	CALL	OUTDECA2	;
+	LD	C,':'		; Display ':'
+	CALL	CONOUT		;
+	PUSH	HL		; Save disks mapping table pointer
+	LD	A,(HL)		; Get image number
+	LD	L,A		;   to HL
+	LD	H,0		;
+	ADD	HL,HL		; Multiply by 16
+	ADD	HL,HL		;
+	ADD	HL,HL		;
+	ADD	HL,HL		;
+	LD	DE,DSKNAMES	; Add vol names table origin
+	ADD	HL,DE		;
+	CALL	PUTS		; Display vol name
+	LD	C,' '		; Pad to 8 chars
+TABSL1	CALL	CONOUT		; Display ' '
+	INC	L		; Increment counter
+	BIT	3,L		; reached 8 ?
+	JR	Z,TABSL1	; Loop if not
+	POP	HL		; Restore disks mapping table pointer
+	LD	C,'?'		; Display '?'
+	CALL	CONOUT		;
+DSELKEY	CALL	CONIN		; Get char from keyboard
+	CP	CR		; <RET> pressed ?
+	JR	Z,DSELNXT	; Go and accept if yes
+	AND	0DFH		; Convert to upper case
+	LD	C,10		; 10 to add
+	CP	'T'		;   if 'T' pressed
+	JR	Z,DSELADD	; go if yes
+	LD	C,1		; 1 to add
+	CP	'U'		;   if 'U' pressed
+	JR	NZ,DSELKEY	; loop if not
+DSELADD	LD	A,(HL)		; Add 1 or 10 to the vol number
+	ADD	A,C		;
+	LD	(HL),A		;
+	SUB	100		; Greater than 99 ?
+	JR	C,DSELNSB	; Subtract 100 if yes
+	LD	(HL),A		;
+DSELNSB	POP	BC		; Restore counters
+	JR	LOOPSEL		; Loop again displaying new vol name
+;	Accept vol number
+DSELNXT	LD	C,CR		; Go to next screen line
+	CALL	CONOUT		;
+	LD	C,LF		;
+	CALL	CONOUT		;
+	POP	BC		; Restore counters
+	INC	HL		; Increment disk mapping table ptr
+	INC	C		; Increment logical unit number
+	LD	A,C		;
+	CP	6		; If logical unit number is 6
+	JR	NZ,DSELNX1	;
+	LD	C,9		;   change it to 9
+DSELNX1	DJNZ	LOOPSEL		; and loop again
+
+	LD	C,LF		; Move to next screen line
+	CALL	CONOUT		;
+
+;	Now ask keyboard mapping code
+LOOPKB:	LD	HL,LAYOUT$	; Display 'Keyboard layout ...'
+	CALL	PUTS		;
+	LD	A,(KEYMAP)	; Get kb mapping code
+	LD	C,A		;
+	CALL	CONOUT		; Display it
+	LD	C,'?'		; Display '?'
+	CALL	CONOUT		;
+LOOPK1:	CALL	CONIN		; Get char from keyboard
+	CP	CR		; <RET> ?
+	JR	Z,CFGSAVE	; If yes, accept
+	AND	0DFH		; Convert to upper case
+	CP	'A'		; Is it a letter ?
+	JR	C,LOOPK1	; Loop again if not
+	CP	'Z'+1		;
+	JR	NC,LOOPK1	;
+	LD	(KEYMAP),A	; Save keyboard mapping code
+	JR	LOOPKB		; and loop again
+
+;	Save new config to DISK0 track 0's last sector
+CFGSAVE	LD	HL,DSKTBL	; Copy disk mapping table
+	PUSH	IX		;
+	POP	DE		;   to config buffer
+	LD	BC,4		; 4 bytes to copy
+	LDIR			;
+	LD	A,(KEYMAP)	; Get keyboard mapping code
+	LD	(DE),A		; Save to config buffer
+	LD	A,DISK0		; Map slot 4 to DISK0
+	LD	(DSKTMP),A	;
+	LD	C,4		; Select slot 4
+	CALL	SELDSK		;
+	LD	C,0		; Select track 0
+	CALL	SETTRK		;
+	LD	C,128		; Select track 0's last sector
+	CALL	SETSEC		;
+	LD	(IX+127),0A5H	; Store signature byte in last pos
+	PUSH	IX		; Get config buffer's address
+	POP	BC		;
+	CALL	SETDMA		; Set buffer address
+	CALL	WRITE		; Write sector
+
+	RET			; Done, proceed loading secondary bootstrap.
+
+;-----------------------------------------------------------------------------
+;	OUT 'A' IN DECIMAL
+OUTDECA2:
+	PUSH	BC		; Save BC
+	LD	B,10		; Display tens
+	CALL	OUTDEC1		;
+	ADD	A,'0'		; Display units
+	LD	C,A		;
+	CALL	CONOUT		;
+	POP	BC		; Restore BC
+	RET			; Done
+
+OUTDEC1:
+	LD	C,'0'-1		; Init tens
+OUTDEC11:
+	INC	C		; Increment tens
+	SUB	B		; Try and Subtract 10
+	JR	NC,OUTDEC11	; Loop while OK
+	ADD	A,B		; add 10 if A<0
+	LD	B,A		; Save to B
+	CALL	CONOUT		; Display tens
+	LD	A,B		; Restore from B
+	RET			; Done
+
+;-----------------------------------------------------------------------------
+DOCFG$	DB	LF,'Press USER key while booting to change system setup.',CR,LF,0
+
+SCNDSK$	DB	LF,CR,'Scanning Volumes: [',0
+
+AVAIL$	DB	LF,CR,LF,'Available volumes:',CR,LF,0
+
+SELDSK$	DB	CR,LF,LF,'Select volume numbers (<T>ens, <U>nits, <ret> to accept):',CR,LF,0
+
+LAYOUT$	DB	CR,'Select keyboard layout (<ret> to accept):',0
+
+;-----------------------------------------------------------------------------
+	DS	1 + low not $	; go to next 256 bytes boundary
+CFGBUF:	DS	128		; Sector buffer for config
+DIRBUF:	DS	128		; Sector buffer for directory
+DSKNAMES:
+	DS	100*16		; Disk names table
 
 ;-----------------------------------------------------------------------------
 ;	CBIOS area begin, to move in high memory at first IPL
@@ -208,7 +517,7 @@ BIOS	EQU	1500H+BIAS	; base of bios
 
 ;-----------------------------------------------------------------------------
 ;
-;	jump vector for individual subroutines
+;	Jump vector for individual subroutines
 ;
 	JP	CBOOT		; cold start
 WBOOTE: JP	WBOOT		; warm start
@@ -292,7 +601,7 @@ LASTKEY	EQU	$-1		;
 	JR	NZ,CONIN1	; Go if not
 	IN	A,(1)		; Read char from term kbd
 	INC	A		; Is there a char ?
-	RET	Z		; done if yes, returning A=0
+	RET	Z		; done if no, returning A=0
 	DEC	A		; recover char
 	AND	0DFH		; Convert letter to upper case
 	LD	(KEYMAP),A	; Save as keymap code
@@ -353,7 +662,7 @@ CONOUT:
 	OUT	(1),A		; Send IOS opcode
 	LD	A,C		; Get to accumulator
 	OUT	(0),A		; Send character to console
-	CP	27		; ESC character?
+	CP	ESC		; ESC character?
 	RET	NZ		; No, done
 	LD	A,01H		; SERIAL_TX
 	OUT	(1),A		; Send IOS opcode
@@ -401,9 +710,14 @@ HOME:	LD	C,0		; select track 0
 ;
 ;	select disk given by register C
 ;
-SELDSK: LD	HL,0000H	; return code
-	LD	A,C		; Save disk number
+SELDSK: LD	A,B		; save B
+	LD	B,0		; clear it
+	LD	HL,DSKTBL	; Get phys disk table pointer
+	ADD	HL,BC		; Add logical disk number
+	LD	B,A		; restore B
+	LD	A,(HL)		; get physical disk # from table
 	LD	(NEWDSK),A	; to be used later when actual I/O ops are performed
+	LD	HL,0000H	; return code
 	XOR	A		; return A=0 for OK
 	RET			; done
 
@@ -498,8 +812,8 @@ NEWSEC	EQU	$-1		;
 	CP	(HL)		; Are they the same ?
 	JR	Z,NOREAD	; Don't flush and read if yes
 
-	; Dirty sector write is now done if needed
-	; Flush the current sector if buffer is dirty
+;	Dirty sector write is now done if needed
+;	Flush the current sector if buffer is dirty
 FLUSH:
 	LD	A,(DIRTY)	; Get the dirty flag
 	OR	A		; Is it dirty ?
@@ -513,15 +827,16 @@ FLUSH:
 	LD	A,09H		; IOS:SELDISK opcode
 	OUT	(1),A		; Send IOS opcode
 	LD	A,(HL)		; Get disk #
-	ADD	A,DISK0		; Add DISK 0 offset for DSnDxx.DSK file name
 	OUT	(0),A		; Send disk # with offset
+	CALL	RETSTAT		; Check error code
+	JR	NZ,WAITIOX	; Go if error on disk select
 
-	; Disk selection now done if needed
+;	Disk selection now done if needed
 NOSELDSK:
 	CALL	READ512		; Read sector
 	JR	NZ,WAITIOX	; Go if error on read
 
-	; Sector now read if needed (currently no check for allocation)
+;	Sector now read if needed (currently no check for allocation)
 NOREAD:
 	LD	DE,BUFFER	; Get 512-byte buffer address in DE
 	LD	A,0		; Get sector offset in A (0..3)
@@ -541,7 +856,7 @@ IO_OP	EQU	$-1		; I/O direction
 	LDIR			; transfer 128 bytes sector from buffer to DMA
 	JR	WAITIOX		; return OK (shows Z)
 
-	; Code segment to write a sector from DMA to the buffer
+;	Code segment to write a sector from DMA to the buffer
 WRITEIO:
 	LD	(DIRTY),A	; Set the buffer "dirty" (needs to be written to disk)
 	EX	DE,HL		; Swap DMA and buffer address
@@ -550,7 +865,7 @@ WRITEIO:
 	LD	(KBTIMER),BC	;
 	XOR	A		; return OK (shows Z)
 
-	; Restore registers and return A=0 if Z or A=1 if NZ
+;	Restore registers and return A=0 if Z or A=1 if NZ
 WAITIOX:
 	POP	BC		; restore regs
 	POP	DE		;
@@ -561,7 +876,8 @@ WAITIOX:
 	RET			; done
 
 
-	; Routine to read a physical sector of 512 bytes to the buffer
+;-----------------------------------------------------------------------------
+;	Routine to read a physical sector of 512 bytes to the buffer
 READ512:
 	LD	A,0AH		; IOS:SELTRACK opcode
 	OUT	(1),A		; send opcode
@@ -582,7 +898,7 @@ READ512:
 	OUT	(1),A		; send opcode
 	INIR			; read first 256 bytes
 	INIR			; read last 256 bytes
-	; Check the error status of the I/O operation
+;	Check the error status of the I/O operation
 RETSTAT:
 	LD	A,85H		; IOS:ERRDISK opcode
 	OUT	(1),A		; send opcode
@@ -592,7 +908,8 @@ RETSTAT:
 	LD	A,1		; otherwise return A=1
 	RET			; done ; return to p-System
 
-	; Routine to write a physical sector of 512 bytes from the buffer
+;-----------------------------------------------------------------------------
+;	Routine to write a physical sector of 512 bytes from the buffer
 WRITE512:
 	XOR	A		; IOS:USER_LED opcode
 	OUT	(1),A		; send opcode
@@ -621,7 +938,13 @@ WRITE512:
 	OUT	(0),A		; set the LED off
 	JR	RETSTAT		; check the error status and exit
 
-	; Variables for disk I/O
+
+;-----------------------------------------------------------------------------
+;	Variables for disk I/O
+
+;	Disk mapping table for drives 0-3
+DSKTBL	DB	DISK0, DISK0+1, DISK0+2, DISK0+3
+DSKTMP	DB	0		; Temp disk number for drive 4
 CURDSK	DB	0FFH		; current disk #
 CURTRK	DW	0FFFFH		; current track #
 CURSEC	DB	0FFH		; current sector #
