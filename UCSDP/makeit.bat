a6f59f69db8bdcd3c9938eafd822e33ccc1466ff
GmEsoft <michel.bernard@advalvas.be>
2019-12-30 13:00:03 +0100

    Update makeit.bat
    makeit.bat: build datetime.asm using CMD commands if datetime.exe not present

--- UCSDP/makeit.bat
+++ UCSDP/makeit.bat
@@ -1,6 +1,15 @@
 @echo off
 set name=ucsdp
-datetime>datetime.asm
+if exist datetime.exe (
+	datetime>datetime.asm
+) else (
+	echo $DATE	MACRO>datetime.asm
+	echo 	DB	"%DATE%">>datetime.asm
+	echo 	ENDM>>datetime.asm
+	echo $TIME	MACRO>>datetime.asm
+	echo 	DB	"%TIME%">>datetime.asm
+	echo 	ENDM>>datetime.asm
+)
 zmac\zmac %name%.asm --od %name% --oo cim,lst -c -s -g
 if errorlevel 1 pause && goto :eof
 copy %name%\%name%.cim autoboot.bin
